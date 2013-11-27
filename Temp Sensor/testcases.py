aadc409f5753ea77b22a79f61380f685c43089f4
Riyadh <riyadh5@live.com>
2013-11-26 20:48:58 -0500

    Update testcases.py

--- Temp Sensor/testcases.py
+++ Temp Sensor/testcases.py
@@ -8,16 +8,16 @@ from allinone import red
 from allinone import setup
 import wiringpi2 as wiringpi
 from time import sleep
-#below you will find 4 test cases to show if the called method does what it should do
+#below you will find 4 test cases to show if the called method correctly functioning
 
-exvalue="Expected Result: flashing Green "
+exvalue="Expected Result: only the Green Led is flashin "
                                                                                                 
 print "\n setup will be called "+exvalue+"\n"
 
 
 setup()
 #second case temp=50
-exvalue="Expected Result: Red on and Green off "
+exvalue="Expected Result: only the Red Led is flashing"
                                                                                                 
 print "\n Led_on with t= 50 will be called "+exvalue+"\n"
 
@@ -27,7 +27,7 @@ Led_con(t)
 
 #third case temp=23
 
-exvalue="Expected Result: Red off and Green on "
+exvalue="Expected Result: only the green Led is on. "
                                                                                                 
 print "\n Led_on with t= 23 will be called "+exvalue+"\n"
 
@@ -35,7 +35,7 @@ t=23
 Led_con(t)
 sleep(5)
 #forth case cleanup no led on ; flashing red and green
-exvalue="Expected Result: flashing Green and red "
+exvalue="Expected Result: the Red and the green Leds are flashin."
 print "\n cleanup will be called "+exvalue+"\n"
 cleanup()
 
